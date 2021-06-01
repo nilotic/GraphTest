cb0b18a6186d23efb972f336720424aea1e48fbd
Den Jo <nilotic2@gmail.com>
2021-06-01 17:39:07 +0900

    Added a Ripple View

---
+++ GraphTest/Graph3/View/RippleView.swift
@@ -0,0 +1,65 @@
+// 
+//  RippleView.swift
+//
+//  Created by Den Jo on 2021/06/01.
+//  Copyright © nilotic. All rights reserved.
+//
+
+import SwiftUI
+
+struct RippleView: View {
+    
+    // MARK: - Value
+    // MARK: Private
+    @State private var radius: CGFloat = 10
+    @State private var isAnimated = false
+    
+
+    // MARK: - View
+    // MARK: Public
+    var body: some View {
+        Circle()
+            .fill(Color.clear)
+            .frame(width: radius, height: radius)
+            .overlay(grayOverlay)
+            .overlay(whiteOverlay)
+            .opacity(isAnimated ? 0.1 : 1)
+            .onAppear {
+                withAnimation(Animation.linear(duration: 3).repeatForever(autoreverses: false)) {
+                    radius = 300
+                    isAnimated = true
+                }
+            }
+    }
+    
+    // MARK: Private
+    private var grayOverlay: some View {
+        Circle()
+            .stroke(Color.gray, lineWidth: 5)
+            .blur(radius: 3)
+            .offset(x: 2, y: 2)
+    }
+    
+    private var whiteOverlay: some View {
+        Circle()
+            .stroke(Color.white, lineWidth: 5)
+            .blur(radius: 3)
+            .offset(x: -2, y: -2)
+    }
+}
+
+
+#if DEBUG
+struct RippleView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        let view = RippleView()
+        
+        Group {
+            view
+                .previewDevice("iPhone 8")
+                .preferredColorScheme(.dark)
+        }
+    }
+}
+#endif
